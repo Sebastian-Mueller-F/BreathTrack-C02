8fddd7164b7bcd75b3972ac01748cf3e29a8f5b4
Sebastian-M-F <92372178+Sebastian-M-F@users.noreply.github.com>
2024-08-18 12:25:51 +0200

    add sensor simulator: iteration 1

---
+++ BreathTracker/sensorsimulator.cpp
@@ -0,0 +1,40 @@
+#include "sensorsimulator.h"
+#include <QRandomGenerator>
+#include <QDebug>
+
+SensorSimulator::SensorSimulator(QObject *parent, double baseline, double amplitude )
+    : QObject{parent},
+    baseline(baseline),
+    amplitude(amplitude)
+
+{
+    generateSensorValuesInInterval(1000);//generate sensor values every second
+
+    //test readout created values
+    connect(this, &SensorSimulator::newCo2Value, [&](double sensorValue) {
+        qDebug() << " Sensor Co2 value : " << sensorValue;
+    });
+}
+
+//Create function for starting the simulation
+
+void SensorSimulator::generateSensorValuesInInterval(int interval)
+{
+    timer.reset(new QTimer(this));
+    connect(timer.data(), &QTimer::timeout, this, &SensorSimulator::generateNewCo2Value);
+    timer.data()->start(interval);
+}
+
+void SensorSimulator::generateNewCo2Value()
+{
+    //generate random value between -1.0 and + 1.0
+    double random = QRandomGenerator::global()->generateDouble(); // val inbetween 0 and 0.999
+    double transformedValue = 2.0 * random - 1.0; //val inbetween -1 and 1
+
+    //simulated value
+    double sensorValue = baseline + (transformedValue * amplitude);
+
+    emit newCo2Value(sensorValue);
+
+}
+
---
+++ BreathTracker/sensorsimulator.h
@@ -0,0 +1,30 @@
+#ifndef SENSORSIMULATOR_H
+#define SENSORSIMULATOR_H
+
+#include <QObject>
+#include <QTimer>
+
+class SensorSimulator : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SensorSimulator(QObject *parent = nullptr, double baseline = 50.0, double amplitude = 10.0 );
+
+signals:
+    void newCo2Value(double value);
+
+private slots:
+    void generateSensorValuesInInterval(int interval);
+    void generateNewCo2Value();
+
+private :
+    //
+    QScopedPointer<QTimer> timer;
+    double baseline;
+    double amplitude;
+
+
+signals:
+};
+
+#endif // SENSORSIMULATOR_H
