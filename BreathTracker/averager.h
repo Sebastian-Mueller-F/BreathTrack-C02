f70c0b1d12c68ddcbc1c99555b2dfa7a46ad980b
Sebastian-M-F <92372178+Sebastian-M-F@users.noreply.github.com>
2024-08-21 11:30:32 +0200

    delete unecessary comments

--- BreathTracker/averager.h
+++ BreathTracker/averager.h
@@ -9,19 +9,17 @@ class Averager : public QObject {
 
 public:
     explicit Averager(QObject *parent = nullptr) : QObject(parent) {}
-
-    // Virtual destructor to ensure proper cleanup of derived classes
     virtual ~Averager() {}
 
-    // Pure virtual function to calculate the average
+    // calculate the average
     virtual double calculate() = 0;
 
-    // Optionally, a method to get and set the period (number of data points)
+    //a method to get and set the period (number of data points)
     virtual size_t getPeriod() const = 0;
     virtual void setPeriod(size_t period) = 0;
 
 signals:
-    // Signal to notify when a new average is calculated
+
     void averageUpdated(double newAverage);
 };
 
