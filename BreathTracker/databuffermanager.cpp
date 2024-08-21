7f171088a1495ecdb6ce5c23493f7b4e7305a968
Sebastian-M-F <92372178+Sebastian-M-F@users.noreply.github.com>
2024-08-21 14:28:43 +0200

    correct sensor data type in Data Buffer Manager

--- BreathTracker/databuffermanager.cpp
+++ BreathTracker/databuffermanager.cpp
@@ -1,9 +1,9 @@
 #include "databuffermanager.h"
 
 DataBufferManager::DataBufferManager() {
-    _buffers[RAW] = new CircularBuffer(_rawCapacity);
-    _buffers[SMA] = new CircularBuffer(_averageCapacity);
-    _buffers[EMA] = new CircularBuffer(_averageCapacity);
+    _buffers[SensorDataType::RAW] = new CircularBuffer(_rawCapacity);
+    _buffers[SensorDataType::SMA] = new CircularBuffer(_averageCapacity);
+    _buffers[SensorDataType::EMA] = new CircularBuffer(_averageCapacity);
 }
 
 CircularBuffer *DataBufferManager::getBuffer(SensorDataType type)
