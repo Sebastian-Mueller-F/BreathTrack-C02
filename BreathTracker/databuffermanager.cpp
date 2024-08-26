096f6735cea94f043bccb338916b22d84045bc0c
Sebastian-M-F <92372178+Sebastian-M-F@users.noreply.github.com>
2024-08-26 18:09:09 +0200

    change BufferSubscription to member variable and pointer so that the life cycle extends the constructor and add print for debugging

--- BreathTracker/databuffermanager.cpp
+++ BreathTracker/databuffermanager.cpp
@@ -1,29 +1,32 @@
 #include "databuffermanager.h"
 #include <QDebug>
+#include <QTimer>
+#include <QMutexLocker>
 
 std::shared_ptr<DataBufferManager> DataBufferManager::_instance = nullptr;
 
-DataBufferManager::DataBufferManager(QObject *parent) : QObject(parent) {
-    _buffers[SensorDataType::RAW] = new CircularBuffer(_rawCapacity);
-    _buffers[SensorDataType::SMA] = new CircularBuffer(_averageCapacity);
-    _buffers[SensorDataType::EMA] = new CircularBuffer(_averageCapacity);
+DataBufferManager::DataBufferManager(QObject *parent) : QObject(parent)
+{
 
+    initializeBuffers();
+    //
+    _rawTrendBufferSubscription = std::make_shared<BufferSubscription>(*_buffers[SensorDataType::RAW]);
+    _smaTrendBufferSubscription = std::make_shared<BufferSubscription>(*_buffers[SensorDataType::SMA]);
+    _emaTrendBufferSubscription = std::make_shared<BufferSubscription>(*_buffers[SensorDataType::EMA]);
 
     //data input for buffers
     connect(SensorSimulator::instance(), &SensorSimulator::newCo2Value, this, &DataBufferManager::onNewData);
+    connect (SMAAverager::instance().data(), &SMAAverager::averageUpdated, this, &DataBufferManager::onNewData);
     connect (EMAAverager::instance().data(), &EMAAverager::averageUpdated, this, &DataBufferManager::onNewData);
 
     //data output for subscribers
     QSharedPointer<TrendDataAPI> trendFrontend = TrendDataAPI::instance();
-    BufferSubscription rawTrendBufferSubscription(*_buffers[SensorDataType::RAW]);
-    BufferSubscription smaTrendBufferSubscription(*_buffers[SensorDataType::SMA]);
-    BufferSubscription emaTrendBufferSubscription(*_buffers[SensorDataType::EMA]);
-
     //make trendAPI class a subscriber of the buffers
     //TODO: retrieve lookbackPeriod from Frontend
-    rawTrendBufferSubscription.registerSubscriber(trendFrontend, 60000);
-    smaTrendBufferSubscription.registerSubscriber(trendFrontend, 60000);
-    emaTrendBufferSubscription.registerSubscriber(trendFrontend, 60000);
+    _rawTrendBufferSubscription->registerSubscriber(trendFrontend, 30000, SensorDataType::RAW);
+    _smaTrendBufferSubscription->registerSubscriber(trendFrontend, 30000, SensorDataType::SMA);
+    _emaTrendBufferSubscription->registerSubscriber(trendFrontend, 30000, SensorDataType::EMA);
+
 
 }
 
@@ -44,8 +47,44 @@ CircularBuffer *DataBufferManager::getBuffer(SensorDataType type)
 
 void DataBufferManager::onNewData(double newData, SensorDataType sensorDataType)
 {
-    qDebug()<<" New Data for Data Buffer from " << sensorDataType << ", data: " << newData;
+    // qDebug()<<" New Data for Data Buffer from " << sensorDataType << ", data: " << newData;
     _buffers[sensorDataType]->writeNewItem(newData);
 
+}
+
+void DataBufferManager::initializeBuffers()
+{
+    _buffers[SensorDataType::RAW] = new CircularBuffer(_rawCapacity, 1000);
+    _buffers[SensorDataType::SMA] = new CircularBuffer(_averageCapacity, 1000);
+    _buffers[SensorDataType::EMA] = new CircularBuffer(_averageCapacity, 1000);
+
+    // Ensure all buffers are initialized correctly
+    if (!_buffers[SensorDataType::RAW] || !_buffers[SensorDataType::SMA] || !_buffers[SensorDataType::EMA]) {
+        qDebug() << "Error: One or more buffers are not initialized correctly!";
+    }
+}
+
+void DataBufferManager::printBufferValues()
+{
+    // RAW buffer values
+            std::vector<double> rawValues = _buffers[SensorDataType::RAW]->readLastNValues(100);
+    qDebug() << "RAW Buffer Values:";
+    for (const double &value : rawValues) {
+        qDebug() << value;
+    }
+
+    // SMA buffer values
+    std::vector<double> smaValues = _buffers[SensorDataType::SMA]->readLastNValues(100);
+    qDebug() << "SMA Buffer Values:";
+    for (const double &value : smaValues) {
+        qDebug() << value;
+    }
+
+    // EMA buffer values
+    std::vector<double> emaValues = _buffers[SensorDataType::EMA]->readLastNValues(100);
+    qDebug() << "EMA Buffer Values:";
+    for (const double &value : emaValues) {
+        qDebug() << value;
+    }
 }
 
