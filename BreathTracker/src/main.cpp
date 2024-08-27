8451f0016400915e8b1e2fd3d852bc031a6fd7eb
Sebastian-M-F <92372178+Sebastian-M-F@users.noreply.github.com>
2024-08-27 15:10:32 +0200

    correct last commit

--- BreathTracker/src/main.cpp
+++ BreathTracker/src/main.cpp
@@ -73,7 +73,7 @@ int main(int argc, char *argv[]) {
   LiveDataAPI::instance();
   TrendDataAPI::instance();
 
-  const QUrl url(QStringLiteral("qrc:/BreathTracker/main.qml"));
+  const QUrl url(QStringLiteral("qrc:/BreathTracker/qml/main.qml"));
   QObject::connect(
       &engine, &QQmlApplicationEngine::objectCreated, &app,
       [url](QObject *obj, const QUrl &objUrl) {
