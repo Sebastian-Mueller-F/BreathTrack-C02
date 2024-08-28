ed3a01d2e1bfe2a5c7f915db07d4a3865f397d2f
Sebastian-M-F <92372178+Sebastian-M-F@users.noreply.github.com>
2024-08-28 12:53:18 +0200

    adapt comment

--- BreathTracker/inc/i_subscriber.h
+++ BreathTracker/inc/i_subscriber.h
@@ -8,13 +8,13 @@
 class I_Subscriber {
 
 public:
-  // No constructor due to double inheritance, and no initialization needed here
+    // No constructor or destructor due to double inheritance, and no initialization needed here
+    // virtual ~Subscriber() {}
 
-  // virtual ~Subscriber() {}
-
-  // Pure virtual function to be implemented by all subscribers
-  virtual void onNewData(const std::vector<double> &data,
-                         SensorDataType type = SensorDataType::RAW) = 0;
+    // Pure virtual function to be implemented by all subscribers
+    virtual void onNewData(const std::vector<double> &data,
+                           SensorDataType type = SensorDataType::RAW)
+        = 0;
 };
 
 #endif // SUBSCRIBER_H
